f3bc5ddeab738a0fbaa03a1c9ca35e100591ddea
Nhkien <nhkien.cmc@gmail.com>
2022-11-02 09:34:43 +0700

    first commit

---
+++ spring-aws-02/src/test/java/com/example/springaws02/SpringAws02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springaws02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAws02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
