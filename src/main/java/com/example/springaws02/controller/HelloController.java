a39c219117b79ab61607f081c1db7cfff4b48941
PhucPham96 <phucpham0396@gmail.com>
2022-11-02 09:44:35 +0700

    first commit

---
+++ src/main/java/com/example/springaws02/controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.springaws02.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public ResponseEntity<String> init() {
+        return ResponseEntity.ok("Hello");
+    }
+}
